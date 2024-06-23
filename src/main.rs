06136d2ed756fa2fafa920ed4eea7ab849c7cd8c
Travis Tarpley <34798887+fusionxx23@users.noreply.github.com>
2024-06-22 20:49:18 -0700

    uncomment

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 // Uncomment this block to pass the first stage
-// use std::net::TcpListener;
+use std::net::TcpListener;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
-    
+
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    
+
     for stream in listener.incoming() {
         match stream {
             Ok(_stream) => {
