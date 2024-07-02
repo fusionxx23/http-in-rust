c2ab49e93863d614991c34139a2d9767f82925d8
Travis Tarpley <34798887+fusionxx23@users.noreply.github.com>
2024-07-02 10:18:24 -0700

    feat: Echoing path

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 // Uncomment this block to pass the first stage
 use std::{
-    io::{Read, Write},
+    io::{BufRead, Read, Write},
     net::{TcpListener, TcpStream},
+    str
 };
 
 fn main() {
@@ -11,7 +12,7 @@ fn main() {
     // Uncomment this block to pass the first stage
 
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-
+    
     for stream in listener.incoming() {
         match stream {
             Ok(_stream) => {
@@ -26,16 +27,33 @@ fn main() {
 }
 //
 fn handle_stream(mut stream: TcpStream) {
-    let mut buffer = [0; 512];
+    let mut buffer =[0; 512];
     stream.read(&mut buffer).unwrap();
-
-    let get = b"GET / HTTP/1.1\r\n";
-    let resp = if buffer.starts_with(get) {
-        "HTTP/1.1 200 OK\r\n\r\n"
-    } else {
-        "HTTP/1.1 404 Not Found\r\n\r\n"
+    let a  = match String::from_utf8(buffer.to_vec()) {
+        Ok(v) => v, 
+        _ => {
+            panic!("Error");
+        },
     };
-    print!("HELLO");
+    let b = a.split(' ').collect::<Vec<&str>>();
+    let method = b[0]; 
+    let path = b[1]; 
+    let scheme = b[2];
+    let resp: mut &str = "HTTP/1.1 404 Not Found\r\n\r\n"; 
+    
+    if method == "GET"  { 
+        if scheme.starts_with("HTTP/1.1\r\n") {
+            let path_vec = path.split("/").collect::<Vec<&str>>();
+            if path_vec[1] == "echo" {
+               
+                let content_length  = path_vec[2].len().to_string();
+                if(content_length > 0) { resp = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:" 
+                    + content_length + "\r\n\r\n" + path_vec[2];
+                }
+            }
+        }
+    }
+
     //
     stream.write(resp.as_bytes()).unwrap();
     stream.flush().unwrap()
