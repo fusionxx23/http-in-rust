3e276f9b657733e4548af2b0c61a6af1c95cfb19
Travis Tarpley <34798887+fusionxx23@users.noreply.github.com>
2024-07-03 20:33:28 -0700

    feat: Added body and headers to HttpRequest struct, parse header and body

--- src/http_request.rs
+++ src/http_request.rs
@@ -30,24 +30,60 @@ impl Method {
     }
 }
 
-pub struct HttpRequest {
+pub struct HttpRequest<'a> {
     pub path: String,
     pub method: Method,
     pub scheme:String,
+    pub body:String,
+    pub headers: Vec<&'a str>,
 }
 
-impl HttpRequest { 
-     pub fn new(a:String) -> Result<Self,MethodError> { 
-        let b = a.split(' ').collect::<Vec<&str>>();
-        let method = Method::from_str(b[0])?;
-        let path = b[1]; 
-        let scheme = b[2];
+impl<'a> HttpRequest<'a>{ 
+    pub fn new(a:&'a str) -> Result<Self,MethodError> { 
+        let blocks = a.split("\r\n").collect::<Vec<&'a str>>();
+        if blocks.len() < 1 {
+            return Err(MethodError);
+        }
+
+        let req_params = blocks[0].split(' ').collect::<Vec<&str>>(); 
+        if req_params.len() < 3 {
+            return Err(MethodError)
+        }
+        let method = Method::from_str(req_params[0])?;
+        let path = req_params[1]; 
+        let scheme = req_params[2];
+        let mut headers : Option<Vec<&str>> = None;
+
+        if blocks.len() >= 2 {
+          headers = Some(blocks[1..blocks.len()-2].to_owned());
+        }
+
+       let headers = match headers {
+            Some(i) => i, 
+            None => vec![]
+        };
+
+        // Body should always come last
+        let body = blocks[blocks.len() - 1];
 
        Ok(Self{ 
             path:path.to_owned(),
-            method:method,
+            method,
             scheme:scheme.to_owned(),
+            body: body.to_owned(),
+            headers,
         })
 
     }
+
+
+    pub fn get_header(&self, s:&str) -> Option<&&str> {
+        for header in &self.headers[..] {
+            if header.contains(s) {
+                return Some(header)
+            }
+        };
+        None 
+    }
 } 
+
