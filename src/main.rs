1d62bf6e63238678f461565f20ed9dafa1d67076
Tarp <travistarpley0@gmail.com>
2024-07-18 23:05:27 -0700

    refactor: Moved get_resp to seperate function, removing nested if statements

--- src/main.rs
+++ src/main.rs
@@ -32,6 +32,11 @@ fn get_env_directory() -> Result<String, ()>   {
     Ok(dir.clone())
 }
 fn handle_stream(mut stream: TcpStream) {
+    let resp = get_response(stream.clone());
+    stream.write_all(resp.as_bytes()).unwrap();
+    stream.flush().unwrap()
+}
+fn get_response(mut stream: TcpStream) -> String {
     let mut buffer =  vec![0;2048];
     stream.read(&mut buffer).unwrap();
     let a  = match String::from_utf8(buffer.to_vec()) {
@@ -40,72 +45,65 @@ fn handle_stream(mut stream: TcpStream) {
             panic!("Error");
         },
     };
-    println!("A");
     let request = 
         http_request::HttpRequest::new(&a).unwrap();
-
-    println!("b", );
-    let mut resp: Option<String> = None;
+    let mut resp = "HTTP/1.1 404 Not Found\r\n\r\n".to_owned();
     
-    println!("c" );
     if request.scheme.starts_with("HTTP/1.1") {
         let path_vec = request.path.split('/').collect::<Vec<&str>>();
         if request.method.as_str() == "POST" && path_vec[1] == "files" {
             let file_name = path_vec[2];
-            let dir = get_env_directory();
+            let dir = match get_env_directory() {
+                Ok(i) => i, 
+                Err(_) => {return resp} // TODO RETURN INTERNAL SERVER ERROR
+            };
             let content_length = request.get_header("Content-Length").unwrap_or("");
             let content_length_int: usize = content_length.to_owned().parse().unwrap();
             let bytes = request.body.as_bytes();
-            let byte_slice =   &bytes[0..content_length_int];
-
-            if !file_name.is_empty() && !content_length.is_empty() {
-                if let Ok(dir) = dir { 
-                    let file = File::create(dir + "/" + file_name);
-
-                    if let  Ok(mut file) =  file {
-                        match file.write_all(byte_slice) {
-                            Ok(_) => {
-                                println!("Success writing file");
-                                resp = Some("HTTP/1.1 201 Created\r\n\r\n".to_owned());
-                            },
-                            Err(error) => {println!("Error: {}", error)}
-                        }             
-                    } 
-                } 
+            let byte_slice = &bytes[0..content_length_int];
+            if file_name.is_empty() || content_length.is_empty() {
+                return resp 
             }
+            
+            let file = File::create(dir + "/" + file_name);
+            if let  Ok(mut file) =  file {
+                match file.write_all(byte_slice) {
+                    Ok(_) => {
+                        println!("Success writing file");
+                        resp = "HTTP/1.1 201 Created\r\n\r\n".to_string();
+                    },
+                    Err(error) => {println!("Error: {}", error)}
+                }             
+            } 
+
         }
 
         if request.method.as_str() == "GET"  { 
                 if path_vec[1] == "echo" { 
                     let content_length  = path_vec[2].len();
                     if content_length > 0 {
-                        resp = Some(format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
-                        content_length, path_vec[2]));
+                        resp = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                        content_length, path_vec[2]);
                     }
                 } else if path_vec[1] == "user-agent"{
                     let user_agent = request.get_header("User-Agent");
                     if let Some(x) = user_agent {
-                        resp = Some(http_response::get_text_plain_response(x));
+                        resp = http_response::get_text_plain_response(x);
                     }
                 } else if path_vec[1] == "files"{
                     let file_path = path_vec.get(2);
                     if let Some(file_path) = file_path {
                         let file_resp = http_response::get_file_response(file_path);
                         if let Ok(fr) = file_resp { 
-                        resp = Some(fr.clone());
+                        resp = fr;
                         }
                     }
                 } else if path_vec[1].is_empty() {
-                    resp = Some("HTTP/1.1 200 OK\r\n\r\n".to_owned());
+                    resp = "HTTP/1.1 200 OK\r\n\r\n".to_string();
                 }
         }
     }
+    resp
 
-
-    let resp = match resp {
-        Some(i) => i,
-        None => "HTTP/1.1 404 Not Found\r\n\r\n".to_string()
-    }; 
-    stream.write_all(resp.as_bytes()).unwrap();
-    stream.flush().unwrap()
 }
+
